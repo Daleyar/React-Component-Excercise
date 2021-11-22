7e4bc7a2e9322e746744b4a495af1d1fcea07fd3
Ali Daley <ali.daley72@gmail.com>
2021-11-21 22:25:29 -0500

    created the namelist component to list prop utilizing the map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 import DisplayName from './DisplayName/DisplayName';
+import NamesList from "./NamesList/NamesList";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             firstName: 'Reggie',
-            lastName: 'White'
+            lastName: 'White',
+            names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kellie']
         }
     }
 
@@ -16,6 +18,7 @@ class App extends Component {
                 <h1>
                 <DisplayName firstName={this.state.firstName} />
                 <DisplayName lastName={this.state.lastName} />
+                <NamesList names={this.state.names} />
                 </h1>
             </div>
         );
